226256a78f124ea21aecd93c6101585379eef2ba
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-06 22:23:55 +0600

    admin role

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5001;
 
 //middleware
@@ -40,7 +40,8 @@ async function run() {
       .db("doctorsPortal")
       .collection("appointmentOptions");
     const bookingCollection = client.db("doctorsPortal").collection("bookings");
-    const userCollection = client.db("doctorsPortal").collection("users");
+      const userCollection = client.db("doctorsPortal").collection("users");
+      const doctorCollection = client.db("doctorsPortal").collection("doctors")
 
     //use Aggregate to query multiple collection
     app.get("/appointmentOptions", async (req, res) => {
@@ -64,6 +65,13 @@ async function run() {
       });
       res.send(options);
     });
+      
+      app.get("/appointmentSpecialty", async (req, res) => {
+          const query = {};
+          const result = await appointmentOptionsCollection.find(query).project({ name: 1 }).toArray();
+          res.send(result);
+    })
+      
 
     app.get("/bookings",verifyJWT, async (req, res) => {
       const email = req.query.email;
@@ -78,6 +86,12 @@ async function run() {
       res.send(bookings);
     });
 
+      app.get("/users", async (req, res) => {
+          const query = {};
+          const users = await userCollection.find(query).toArray();
+          res.send(users);
+     })   
+      
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
 
@@ -108,11 +122,70 @@ async function run() {
         }
         const user = await userCollection.findOne(query);
         if (user) {
-            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' });
             return res.send({accessToken: token});
         }
         res.status(401).send({accessToken: ''})
     });
+      
+      //make admin
+      app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+          const decodedEmail = req.decoded.email;
+          const query = {
+              email: decodedEmail
+          }
+          const user = await userCollection.findOne(query);
+          if (user?.role !== 'admin') {
+              return res.status(403).send({message: 'Forbidden access'})
+          }
+          const id = req.params.id;
+          const filter = {
+              _id: ObjectId(id)
+          };
+          const updateDoc = {
+              $set: {
+                  role: 'admin'
+              }
+          }
+          const options = { upsert: true };
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+          res.send(result);
+      })
+      
+      //check admin
+      app.get('/users/admin/:email', async (req, res) => {
+          const email = req.params.email;
+          const query = {
+              email
+          };
+          const user = await userCollection.findOne(query);
+          console.log({ isAdmin: user?.role === 'admin' })
+          res.send({ isAdmin: user?.role === 'admin' });
+          
+      })
+
+      //add doctor
+      app.post("/doctors",  async (req, res) => {
+        //   const decodedEmail = req.decoded.email;
+        //   const query = {
+        //     email: decodedEmail
+        //   }
+        //   const user = await userCollection.findOne(query);
+        //   if (user?.role !== 'admin') {
+        //     return res.status(403).send({message: 'Forbidden access'})
+        // }
+          const doctor = req.body;
+          const result = await doctorCollection.insertOne(doctor);
+          res.send(result);
+      })
+    
+    //get doctors
+    app.get('/doctors', async (req, res) => {
+      const query = {};
+      const doctors = await doctorCollection.find(query).toArray();
+      res.send(doctors)
+    })
+      
   } finally {
   }
 }
