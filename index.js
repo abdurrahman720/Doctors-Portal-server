8668191f603054634c667cbdc270287506897ec0
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-06 11:11:06 +0600

    appointment api

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5001;
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rtntsud.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appointmentOptionsCollection = client.db("doctorsPortal").collection("appointmentOptions");
+        const bookingsCollection = client.db("doctorsPortal").collection("bookings");
+
+        //use Aggregate to query multiple collection
+        app.get("/appointmentOptions", async (req, res) => {
+            const date = req.query.date;
+            console.log(date)
+            const query = {};
+            const options = await appointmentOptionsCollection.find(query).toArray();
+            const bookingQuery = { appointmentDate: date };
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                option.slots = remainingSlots;
+            })
+            res.send(options);
+        })
+
+        app.post("/bookings", async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            console.log(result);
+            res.send(result);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(err => {console.log(err)})
+
 
 app.get('/', (req, res) => {
     res.send("Doctors portal server is running")
